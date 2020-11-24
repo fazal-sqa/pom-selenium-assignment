6e6ec62e873640033b5702a1cb7084411050185d
FazalRehman <fazal.rehman.sqa@gmail.com>
2020-11-24 20:12:23 +0500

    Selenium Selenide POM Task Completed

---
+++ src/main/java/com/base/BaseClass.java
@@ -0,0 +1,30 @@
+package com.base;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass {
+
+	public static WebDriver driver;
+
+	public static void initialization(String browserName) throws InterruptedException, IOException {
+
+		Properties prop = new Properties();
+		FileInputStream ip = new FileInputStream(
+				"D:\\selenium_assignment_pom-master\\selenium_assignment_pom-master\\src\\main\\java\\config.properties");
+		prop.load(ip);
+
+		// Chrome Browser -- WebDriver Configurations
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			String chromeDriverPath = prop.getProperty("chrome_driver_path");
+			System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		}
+
+		open(prop.getProperty("url"));
+	}
+}
